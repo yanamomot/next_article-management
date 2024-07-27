24d7e1e9d18e6f24a085acdd15fcc265cddbce83
Yana Momot <yanamomot2004@gmail.com>
2024-07-27 17:19:31 +0300

    minor fix

--- src/app/admin-panel/page.tsx
+++ src/app/admin-panel/page.tsx
@@ -13,7 +13,7 @@ export default function AdminPanel() {
   const [sortBy, setSortBy] = useState(SortBy.All);
   const [query, setQuery] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [itemsPerPage, setItemsPerPage] = useState(12);
 
   useEffect(() => {
     fetch();
